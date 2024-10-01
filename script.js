aaf9bac9fc468bcd5d025a4c4e9bd318d2367b97
Askar Soronbekov <asko.soronbekov@gmail.com>
2024-10-01 20:39:03 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// document.getElementById('alertButton').addEventListener('click', function() {
+//     alert('Кнопка была нажата!');
+// });
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function() {
+    alert("Что то нужно ?")
+})
